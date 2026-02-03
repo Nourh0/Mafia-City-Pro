c4a0d49c8812c8de981055bcccf66989a02822d7
Nourh0 <nooon-25-25@hotmail.com>
2026-02-03 18:03:06 +0300

    Config.lua
    Updated comments to provide English translations and improved clarity. Added game limits and rewards configurations.

--- Config.lua
+++ Config.lua
@@ -1,23 +1,36 @@
 -- Modules/Config.lua
--- هذا الملف هو المحرك المركزي لتوازنات اللعبة
+-- هذا الملف هو المحرك المركزي لتوازنات اللعبة (Mafia City)
 
 local Config = {}
 
--- [1] إعدادات الأوقات (بالثواني)
+-- [1] إعدادات الأوقات (Time Settings - بالثواني)
 Config.TimeSettings = {
-    NightDuration = 35,   -- مدة الليل
-    DayDuration = 60,     -- مدة النهار
+    NightDuration = 35,   -- مدة الليل (تحركات المافيا)
+    DayDuration = 60,     -- مدة النهار (النقاش والتصويت)
     NewsDuration = 12     -- مدة عرض الأخبار (الجريدة)
 }
 
--- [2] ميكانيكيات ونظام التصويت
+-- [2] ميكانيكيات ونظام التصويت (Voting Mechanics)
 Config.VotingMechanics = {
-    JudgeVoteWeight = 2,   -- قوة صوت القاضي (صوته يحسب عن اثنين)
+    JudgeVoteWeight = 2,   -- قوة صوت القاضي (صوته يعادل صوتين)
     AllowSelfVote = false, -- منع اللاعب من التصويت لنفسه
-    VetoEnabled = true     -- تفعيل خاصية النقض (Veto)
+    VetoEnabled = true     -- تفعيل خاصية النقض (الفيتو)
 }
 
--- [3] تعريف الأدوار الأساسية (7 أدوار ثنائية اللغة)
+-- [3] حدود اللعبة (Game Limits - تحسين احترافي)
+Config.GameLimits = {
+    MinPlayers = 6,        -- الحد الأدنى لبدء الجولة
+    MaxPlayers = 12,       -- الحد الأقصى للسيرفر
+    MafiaCountRatio = 3    -- لاعب مافيا واحد لكل 3 لاعبين (توازن القوى)
+}
+
+-- [4] نظام المكافآت (Rewards - تحسين احترافي)
+Config.Rewards = {
+    WinCoins = 100,        -- عملات مكافأة الفوز
+    ParticipationXP = 50   -- نقاط خبرة للمشاركة
+}
+
+-- [5] تعريف الأدوار الأساسية (7 أدوار ثنائية اللغة)
 Config.Roles = {
     {Id = 1, NameAr = "مواطن", NameEn = "Villager"},
     {Id = 2, NameAr = "مافيا", NameEn = "Mafia"},
@@ -28,8 +41,8 @@ Config.Roles = {
     {Id = 7, NameAr = "عراب", NameEn = "Godfather"}
 }
 
--- [4] ملاحظات برمجية للاستخدام:
--- لاستدعاء هذا الملف من سكريبتات أخرى استخدم السطر التالي:
+-- [6] ملاحظات برمجية للاستخدام:
+-- للوصول إلى هذا الملف من الأنظمة الأخرى (مثل VotingSystem أو RoleDistributor):
 -- local Config = require(game:GetService("ReplicatedStorage").Modules.Config)
 
 return Config
